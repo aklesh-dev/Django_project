75b368ab3f35fb25eb3a28fa0bd0d9961a8489f7
aklesh yadav <arzunayavadu998@gmail.com>
2024-01-02 09:22:24 +0545

    user registration add

--- social_website/bookmarks/account/urls.py
+++ social_website/bookmarks/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -7,18 +7,21 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     
     # login/logout urls
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    # Change password urls
-    path('password-change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
-    path('password-change/done/',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
+    # # Change password urls
+    # path('password-change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
+    # path('password-change/done/',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
 
-    #reset password urls
-    path('password-rest',auth_views.PasswordResetView.as_view(),name='password_reset'),
-    path('password-rest/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('password-rest/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('password-rest/complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    # #reset password urls
+    # path('password-rest',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    # path('password-rest/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    # path('password-rest/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    # path('password-rest/complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 
+    
+    path('', include('django.contrib.auth.urls')),
     path('',views.dashboard, name='dashboard'),
+    path('register/',views.register, name='register'),
 ]
