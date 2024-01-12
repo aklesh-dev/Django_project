6cadb5b9892df9110fa65f28bc63f4f276ac9bc3
aklesh yadav <arzunayavadu998@gmail.com>
2024-01-12 10:31:43 +0545

    added soical auth pipeline

--- social_website/bookmarks/bookmarks/settings.py
+++ social_website/bookmarks/bookmarks/settings.py
@@ -153,3 +153,16 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 # google auth
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '351936009227-9qid7916nv3dcra7lrapks73alij2rss.apps.googleusercontent.com ' # Google Client ID
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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' # Google Client Secret
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
